250d9963d0474341cbfc9468fc0e6c0dfdfda277
Felipe Costa <felipe95176@gmail.com>
2024-03-27 20:29:07 -0300

    Finalmente consegui adicionar estrelas em locais aleatórios e mudar de uma animação de fadeIn para uma animação de rotação infinita. Além disso, estou tentando aumentar a margem do topo da página, mas o céu não está acompanhando.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Cria as nuvens da esquerda
-function createCloudsLeft() {
+function createCloudsLeft() 
+{
     const cloud = document.createElement('div');
     cloud.classList.add('cloudMain');
     cloud.style.height = Math.floor(Math.random() * 4) + 'em';
@@ -11,7 +12,8 @@ function createCloudsLeft() {
     document.body.appendChild(cloud);
 }
 //Cria as nuvens da direita
-function createCloudsRight() {
+function createCloudsRight() 
+{
     const cloud = document.createElement('div');
     cloud.classList.add('cloudMain');
     cloud.style.height = Math.floor(Math.random() * 4) + 'em';
@@ -23,14 +25,13 @@ function createCloudsRight() {
 
     document.body.appendChild(cloud);
 }
-/*Dois for's, cada uma para uma função de nuvem
-diferente, sendo o primeiro para as nuvens da esquerda
-e o segundo para as nuvens da direita*/
-for (let i = 0; i < 8; i++) {
-    createCloudsLeft();
-}
-for (let i = 0; i < 2; i++) {
-    createCloudsRight();
+
+function fullSky() {
+    const sky = document.createElement('div');
+    sky.classList.add('sky');
+    sky.style.height = '300px'; // Altura fixa do céu
+    sky.style.position = 'absolute';
+    document.body.insertBefore(sky, document.body.firstChild); // Insere o céu no início do corpo
 }
 
 // Função para criar estrelas
@@ -38,12 +39,35 @@ function createStar() {
     const star = document.createElement('div');
     star.classList.add('stars');
     star.style.left = Math.floor(Math.random() * window.innerWidth) + 'px';
+    star.style.top = Math.floor(Math.random() * window.innerHeight) + 'px';
+    star.addEventListener('animationend', () => {
+        star.style.animation = 'rotate 6s ease infinite'; // Adiciona a classe 'infinito' após a conclusão da animação fadeIn
+    });
     document.body.appendChild(star);
 }
-//Criar i estrelas
-for (let i = 0; i < 3; i++) {
-    createStar();
+
+
+function adicionarEspaco() {
+    // Adiciona margem superior ao corpo da página
+    document.body.style.marginTop = '600px'; // Altere o valor conforme necessário
+    
+    fullSky();
+    
+    //Criar i estrelas
+    for (let i = 0; i < 7; i++) 
+    {
+        createStar();
+    }
+    /*Dois for's, cada uma para uma função de nuvem
+    diferente, sendo o primeiro para as nuvens da esquerda
+    e o segundo para as nuvens da direita*/
+    for (let i = 0; i < 8; i++) 
+    {
+        createCloudsLeft();
+    }
+    for (let i = 0; i < 2; i++) 
+    {
+        createCloudsRight();
+    }
 }
-
-
 
