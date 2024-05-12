bf95cda1203080553b5156276130593f7951c729
Felipe Costa <felipe95176@gmail.com>
2024-05-12 15:40:09 -0300

    Reorganizei muita coisa, adicionei alguns botões da barra de navegação

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 function returnTop() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 }
-
+function showAboutMe() {
+    returnTop();
+    showAboutMe_underlay = document.querySelector('.showAboutMeUnderlay');
+    showAboutMe_underlay.style.animation = 'showAboutMeUnderlay_purple 1s ease-in-out';
+    showAboutMe_underlay.addEventListener('animationend', () =>{
+        showAboutMe_underlay.style.animation = 'none';
+    });
+}
+function showMyContacts() {
+    returnTop();
+    showMyContacts_underlay = document.querySelector('.showMyContactsUnderlay');
+    showMyContacts_underlay.style.animation = 'showMyContacts_purple 1s ease-in-out';
+    showMyContacts_underlay.addEventListener('animationend', () =>{
+        showMyContacts_underlay.style.animation = 'none';
+    });
+}
 class TextBalloon {
     constructor(top, left, height, width, gmail, whatsapp) {
         this.textBalloonElement = document.createElement('div');
@@ -20,14 +35,14 @@ class TextBalloon {
             this.textBalloon3_Gmail.style.textAlign = 'center';
             this.textBalloon3_Gmail.style.background = 'none';
             this.textBalloon3_Gmail.style.fontFamily = 'Arial';
-            this.textBalloon3_Gmail.style.fontSize = '14px';
+            this.textBalloon3_Gmail.style.fontSize = '16px';
             this.textBalloon3_Gmail.style.left = 50+'%';
             this.textBalloon3_Gmail.style.top = 0+'px';
-            this.textBalloon3_Gmail.style.transform = 'translate(-232%, 433px)';
+            this.textBalloon3_Gmail.style.transform = 'translate(-225%, 468px)';
             
             gmail.addEventListener('click', () => {
                 this.textBalloon3_Gmail.innerHTML = 'E-mail copiado!';
-                this.textBalloon3_Gmail.style.transform = 'translate(-290%, 442px)';
+                this.textBalloon3_Gmail.style.transform = 'translate(-280%, 478px)';
                 
                 const email = 'felipe95176@gmail.com';
                 const clicked = document.createElement('input');
@@ -52,11 +67,10 @@ class TextBalloon {
                     animou = true;
                 });
             });
-            
             gmail.addEventListener('mouseleave', () => {
                 this.textBalloon3_Gmail.style.animation = 'none';
                 this.textBalloon3_Gmail.innerHTML = 'Clique aqui para<br>copiar o meu e-mail!';
-                this.textBalloon3_Gmail.style.transform = 'translate(-232%, 433px)';
+                this.textBalloon3_Gmail.style.transform = 'translate(-225%, 468px)';
                 
                 this.textBalloonElement.classList.remove('animationWhenTheMouseIsIn');
                 if(dentro == true && animou == true) {
@@ -76,14 +90,13 @@ class TextBalloon {
             this.textBalloon4_Whatsapp.style.textAlign = 'center';
             this.textBalloon4_Whatsapp.style.background = 'none';
             this.textBalloon4_Whatsapp.style.fontFamily = 'Arial';
-            this.textBalloon4_Whatsapp.style.fontSize = '14px';
+            this.textBalloon4_Whatsapp.style.fontSize = '16px';
             this.textBalloon4_Whatsapp.style.left = 45+'%';
             this.textBalloon4_Whatsapp.style.top = 0+'px';
-            this.textBalloon4_Whatsapp.style.transform = 'translate(-219%, 498px)';
-
+            this.textBalloon4_Whatsapp.style.transform = 'translate(-215%, 541px)';
             whatsapp.addEventListener('click', () => {
                 this.textBalloon4_Whatsapp.innerHTML = 'Número copiado!';
-                this.textBalloon4_Whatsapp.style.transform = 'translate(-270%, 508px)';
+                this.textBalloon4_Whatsapp.style.transform = 'translate(-260%, 550px)';
                 
                 const numPhone = '(84) 98709-5902';
                 const clicked = document.createElement('input');
@@ -93,9 +106,7 @@ class TextBalloon {
                 document.execCommand('copy');
                 document.body.removeChild(clicked);
             });
-            
             document.body.appendChild(this.textBalloon4_Whatsapp);
-
             let animou = false;
             let dentro = false;
             whatsapp.addEventListener('mouseenter', () => {
@@ -109,11 +120,10 @@ class TextBalloon {
                     animou = true;
                 });
             });
-            
             whatsapp.addEventListener('mouseleave', () => {
                 this.textBalloon4_Whatsapp.style.animation = 'none';
                 this.textBalloon4_Whatsapp.innerHTML = 'Clique aqui para<br>copiar o meu número!';
-                this.textBalloon4_Whatsapp.style.transform = 'translate(-219%, 498px)';
+                this.textBalloon4_Whatsapp.style.transform = 'translate(-215%, 541px)';
                 
                 this.textBalloonElement.classList.remove('animationWhenTheMouseIsIn');
                 if(dentro == true && animou == true) {
@@ -122,8 +132,7 @@ class TextBalloon {
                 dentro = false;
                 animou = false;
                 });
-        }
-
+            }
         }
     addToBody() {
         document.body.appendChild(this.textBalloonElement);
@@ -133,36 +142,36 @@ function textBalloon()
 {
     //Github
     const textBalloon1 = new TextBalloon(
-        -745 + 'px',
-        0 +'px',
+        -670 + 'px',
+        -30 +'px',
         60 + 'px',
         60 + 'px'
     );
 
     //Linkedin
     const textBalloon2 = new TextBalloon(
-        -805 + 'px',
-        -75 +'px',
+        -730 + 'px',
+        -105 +'px',
         60 + 'px',
         60 + 'px'
     );
 
     //Gmail
     const textBalloon3 = new TextBalloon(
-        -900 + 'px',
-        -324 +'px',
-        60 + 'px',
-        60 + 'px',
+        -868 + 'px',
+        -370 +'px',
+        70 + 'px',
+        70 + 'px',
         true
     );
     
     
     //Whatsapp
     const textBalloon4 = new TextBalloon(
-        -895 + 'px',
-        -324 +'px',
-        60 + 'px',
-        60 + 'px',
+        -865 + 'px',
+        -370 +'px',
+        70 + 'px',
+        70 + 'px',
         false,
         true
     );
